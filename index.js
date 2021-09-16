67eba005b16b725fde4ada3b532ab7cde657d49b
almuhandis <abubakaralmuhandis@gmail.com>
2021-09-16 11:32:12 +0100

    laptop store project

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const json = fs.readFileSync(`${__dirname}/data/data.json` , 'utf-8');
+const laptopData = JSON.parse(json);
+console.log(laptopData);
+
+const server = http.createServer((req, res) => {
+const query  = url.parse(req.url, true);
+
+console.log(query);
+
+
+
+
+    res.writeHead(200, {'content-type': 'text/html'});
+   res.end('This is the response!');
+});
+
+server.listen(1337, '127.0.0.1', () =>{
+    console.log('Listening for requests now');
+});
